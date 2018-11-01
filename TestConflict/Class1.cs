4733117268ba7f640bb72422e86e3390b8ed38e5
ctr208 <ctr208@yahoo.com>
2018-10-31 20:46:36 -0400

    first commit

---
+++ TestConflict/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConflict
+{
+    class Course
+    {
+        public string CRN { get; }
+        public string Crse { get; }
+        public string Sec { get; }
+        public string Cmp { get; }
+        public string Cred { get; }
+        public string Title { get; }
+        public string Days{ get; }
+        public string Time { get; }
+        public string SeatsRem { get; }
+        public string WLCAP { get; }
+        public string WLACT { get; }
+        public string WLREM { get; }
+        public string RsrvdRem { get; }
+        public string Instructor { get; }
+        public string Dates{ get; }
+        public string Session { get; }
+        public string Location { get; }
+        public string BitArray { get; }
+
+
+        public Course(string CRN, string Crse, string Sec, string Cmp, string Cred, string Title, string Days, string Time,
+                        string SeatsRem, string WLCAP, string WLACT, string WLREM, string RsrvdRem, string Instructor, string Dates, string Session, string Location, string BitArray)
+        {
+            this.CRN = CRN;
+            this.Crse = Crse;
+            this.Sec = Sec;
+            this.Cmp = Cmp;
+            this.Cred = Cred;
+            this.Title = Title;
+            this.Days = Days;
+            this.Time = Time;
+            this.SeatsRem = SeatsRem;
+            this.WLCAP = WLCAP;
+            this.WLACT = WLACT;
+            this.WLREM = WLREM;
+            this.RsrvdRem = RsrvdRem;
+            this.Instructor = Instructor;
+            this.Dates = Dates;
+            this.Session = Session;
+            this.Location = Location;
+            this.BitArray = BitArray;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ TestConflict/Program.cs
@@ -0,0 +1,89 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+
+namespace TestConflict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DATABASE
+            ReadDataBase();
+
+            //SUBJECTS
+            string subject1 = "";
+            string subject2 = "";
+            string subject3 = "";
+            string subject4 = "";
+            string subject5 = "";
+            string subject6 = "";
+            ArrayList subjectsList = new ArrayList() {};
+
+
+            //COURSENUM
+            string courNum1 = "";
+            string courNum2 = "";
+            string courNum3 = "";
+            string courNum4 = "";
+            string courNum5 = "";
+            string courNum6 = "";
+            ArrayList courseNumList = new ArrayList() { };
+
+            //SECTION***
+            string section1 = "";
+            string section2 = "";
+            string section3 = "";
+            string section4 = "";
+            string section5 = "";
+            string section6 = "";
+            ArrayList sectionsList = new ArrayList() { };
+
+            //SEARCH CRITERIA 
+            Boolean preferedOnline = false;
+            ArrayList preferedDays = new ArrayList() {"M","T","W","R","F","S","U" };
+            ArrayList preferedTimes = new ArrayList() {"Morning", "Noon", "Evening"};
+
+
+            //Generate some data structure to store CRN, SUBJ, COURSENUM, SECTION, and bitArray for each course
+
+
+            string query = "SELECT crn,bitArray FROM course_table WHERE subject = "+subject1+" and coursenum ="+courNum1+"";
+
+
+        }
+
+        private static void ReadDataBase()
+        {
+
+            string queryStr = "Select * FROM Course_Table;";
+            string ConnectionStr = "server=localhost; uid=root; pwd=12345; database=Courses";
+            using (MySqlConnection connection = new MySqlConnection(ConnectionStr))
+            {
+                MySqlCommand command = new MySqlCommand(queryStr, connection);
+                connection.Open();
+                try
+                {
+                    MySqlDataReader reader = command.ExecuteReader();
+
+
+                    while (reader.Read())
+                    {
+                        var value1 = reader.GetValue(0); // On first iteration will be hello
+                        var value2 = reader.GetValue(1); // On first iteration will be hello2
+                        var value3 = reader.GetValue(2); // On first iteration will be hello3
+                    }
+
+                    Console.WriteLine(reader);
+                    Console.ReadLine();
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    connection.Close();
+                }
+
+            }
+        }
+    }
+}
