584dad66168efdfdf5d8370a00019c27bcabfe38
Chase <ctr208@yahoo.com>
2018-11-05 01:37:22 -0500

    created course class

--- TestConflict/Class1.cs
+++ TestConflict/Class1.cs
@@ -6,30 +6,32 @@ namespace TestConflict
 {
     class Course
     {
-        public string CRN { get; }
-        public string Crse { get; }
-        public string Sec { get; }
+        public Int32 CRN { get; }
+        public string Subj { get; }
+        public Int32 Crse { get; }
+        public Int32 Sec { get; }
         public string Cmp { get; }
         public string Cred { get; }
         public string Title { get; }
         public string Days{ get; }
         public string Time { get; }
-        public string SeatsRem { get; }
-        public string WLCAP { get; }
-        public string WLACT { get; }
-        public string WLREM { get; }
-        public string RsrvdRem { get; }
+        public Int32 SeatsRem { get; }
+        public Int32 WLCAP { get; }
+        public Int32 WLACT { get; }
+        public Int32 WLREM { get; }
+        public Int32 RsrvdRem { get; }
         public string Instructor { get; }
         public string Dates{ get; }
         public string Session { get; }
         public string Location { get; }
-        public string BitArray { get; }
+        public object BitArray { get; }
 
 
-        public Course(string CRN, string Crse, string Sec, string Cmp, string Cred, string Title, string Days, string Time,
-                        string SeatsRem, string WLCAP, string WLACT, string WLREM, string RsrvdRem, string Instructor, string Dates, string Session, string Location, string BitArray)
+        public Course(Int32 CRN, string Subj, Int32 Crse, Int32 Sec, string Cmp, string Cred, string Title, string Days, string Time,
+                        Int32 SeatsRem, Int32 WLCAP, Int32 WLACT, Int32 WLREM, Int32 RsrvdRem, string Instructor, string Dates, string Session, string Location, object BitArray)
         {
             this.CRN = CRN;
+            this.Subj = Subj;
             this.Crse = Crse;
             this.Sec = Sec;
             this.Cmp = Cmp;
--- TestConflict/Program.cs
+++ TestConflict/Program.cs
@@ -1,11 +1,14 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace TestConflict
 {
     class Program
     {
+        //ArrayList courseList = new ArrayList();
+        
         static void Main(string[] args)
         {
             //DATABASE
@@ -48,15 +51,16 @@ namespace TestConflict
             //Generate some data structure to store CRN, SUBJ, COURSENUM, SECTION, and bitArray for each course
 
 
-            string query = "SELECT crn,bitArray FROM course_table WHERE subject = "+subject1+" and coursenum ="+courNum1+"";
+            //string query = "SELECT crn,bitArray FROM course_table WHERE subject = "+subject1+" and coursenum ="+courNum1+"";
 
 
         }
 
         private static void ReadDataBase()
         {
+            List<Course> courseList = new List<Course>();
 
-            string queryStr = "Select * FROM Course_Table;";
+            string queryStr = "Select * FROM Course_Table ORDER BY courses.course_table.Subj;";
             string ConnectionStr = "server=localhost; uid=root; pwd=12345; database=Courses";
             using (MySqlConnection connection = new MySqlConnection(ConnectionStr))
             {
@@ -69,10 +73,31 @@ namespace TestConflict
 
                     while (reader.Read())
                     {
-                        var value1 = reader.GetValue(0); // On first iteration will be hello
-                        var value2 = reader.GetValue(1); // On first iteration will be hello2
-                        var value3 = reader.GetValue(2); // On first iteration will be hello3
+                        Int32 crn = (Int32)reader["CRN"]; 
+                        var subj = (string)reader["Subj"]; 
+                        Int32 crse = (Int32)reader["Crse"];
+                        Int32 sec = (Int32)reader["Sec"];
+                        var cmp = (string)reader["Cmp"];
+                        var cred = (string)reader["cred"];
+                        var title = (string)reader["Title"];
+                        var days = (string)reader["Days"];
+                        var time = (string)reader["Time"];
+                        Int32 seatsrem = (Int32)reader["Seats Rem"];
+                        Int32 wlcap = (Int32)reader["WL CAP"];
+                        Int32 wlact = (Int32)reader["WL Act"];
+                        Int32 wlrem = (Int32)reader["WL Rem"];
+                        Int32 rsrvdrem = (Int32)reader["Rsrvd Rem"];
+                        var instructor = (string)reader["Instructor"];
+                        var dates = (string)reader["Dates"];
+                        var session = (string)reader["Session"];
+                        var location = (string)reader["Location"];
+                        var bitarray = reader["BITARRAY"];
+
+                        Course course = new Course(crn, subj, crse, sec, cmp, cred, title, days, time, seatsrem, wlcap, wlact, wlrem, rsrvdrem, instructor, dates, session, location, bitarray);
+                        courseList.Add(course);
                     }
+
+                    var c = courseList.Count;
 
                     Console.WriteLine(reader);
                     Console.ReadLine();
